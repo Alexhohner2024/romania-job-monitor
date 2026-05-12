e357a59c925bfd331ba5e8b1631360b203c90674
Alexhohner2024 <alex.babaian2008@gmail.com>
2026-05-13 00:21:30 +0300

    cleanup: remove temp print sheet files

--- dump_sheet.py
+++
@@ -1,8 +0,0 @@
-import json, os, gspread
-creds = json.loads(os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'])
-client = gspread.service_account_from_dict(creds)
-ws = client.open_by_key(os.environ['GOOGLE_SHEET_ID']).sheet1
-vals = ws.get_all_values()
-print(f'ROWS: {len(vals)}')
-for i,r in enumerate(vals):
-    print(f'{i}: {r}')
--- print_sheet.py
+++
@@ -1,7 +0,0 @@
-import json, os, gspread
-c = json.loads(os.environ['GOOGLE_SERVICE_ACCOUNT_JSON'])
-w = gspread.service_account_from_dict(c).open_by_key(os.environ['GOOGLE_SHEET_ID']).sheet1
-v = w.get_all_values()
-print(f'ROWS: {len(v)}')
-for i, r in enumerate(v):
-    print(f'{i}: {r}')
